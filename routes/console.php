76c0b685f0db5f5752785bb1edbdecd84ccfb613
ratno putro sulistiyono <ratno@knoqdown.com>
2018-07-31 23:13:36 +0700

    add artisan token, to test request token and refresh token

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,36 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('token',function(){
+    $client = new GuzzleHttp\Client();
+    $this->info("Lets get new Access Token");
+    $res = $client->request('POST', 'http://localhost:8000/oauth/token', [
+        'form_params' => [
+            'client_id' => '2',
+            'client_secret' => 'H3HNDkZ2gWh13tZ4z0xwThecfW37m7NIhRULf3mY',
+            'username' => 'ratno@me.com',
+            'password' => '123123',
+            'scope' => '*',
+            'grant_type' => 'password'
+        ]
+    ]);
+
+    $data = json_decode($res->getBody(),true);
+    $this->info("Access Token: ". substr($data['access_token'],0,10) . "..." . substr($data['access_token'],-10));
+    $this->info("Refresh Token: ". substr($data['refresh_token'],0,10) . "..." . substr($data['refresh_token'],-10));
+
+    $this->warn("Lets get Refresh Token");
+    $res = $client->request('POST', 'http://localhost:8000/oauth/token', [
+        'form_params' => [
+            'client_id' => '2',
+            'client_secret' => 'H3HNDkZ2gWh13tZ4z0xwThecfW37m7NIhRULf3mY',
+            'refresh_token' => $data['refresh_token'],
+            'scope' => '*',
+            'grant_type' => 'refresh_token'
+        ]
+    ]);
+    $data = json_decode($res->getBody(),true);
+    $this->warn("Access Token: ". substr($data['access_token'],0,10) . "..." . substr($data['access_token'],-10));
+    $this->warn("Refresh Token: ". substr($data['refresh_token'],0,10) . "..." . substr($data['refresh_token'],-10));
+})->describe('Try to get new access token');
